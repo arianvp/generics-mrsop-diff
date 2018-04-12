815bcb4ab8f0d9be0830366c79157b2eeef1e6cd
Arian van Putten <aeroboy94@gmail.com>
2018-04-12 15:08:31 +0200

    Rename fam to codes

--- src/Generics/MRSOP/GDiff.hs
+++ src/Generics/MRSOP/GDiff.hs
@@ -6,6 +6,7 @@
 
 module Generics.MRSOP.GDiff where
 
+import Data.Semigroup
 import Generics.MRSOP.Base
 import Generics.MRSOP.Util
 type family Append txs tys where
@@ -18,46 +19,56 @@ data SinglCof
   = CofI Nat Nat -- type index and constructor index within the type
   | CofK
 
-data Cof (ki :: kon -> *) (fam :: [[[Atom kon]]]) 
+data Cof (ki :: kon -> *) (codes :: [[[Atom kon]]]) 
          (a :: Atom kon) (c :: SinglCof) where
-  ConstrI :: Constr c (Lkup n fam)  -> Cof ki fam (I n) (CofI n c)
-  ConstrK :: ki k                  -> Cof ki fam (K k) CofK
+  ConstrI :: Constr c (Lkup n codes)  -> Cof ki codes (I n) (CofI n c)
+  ConstrK :: ki k                  -> Cof ki codes (K k) CofK
 
-type family Tyof (fam :: [[[Atom kon]]]) (c :: SinglCof) 
+type family Tyof (codes :: [[[Atom kon]]]) (c :: SinglCof) 
     :: [Atom kon] where
   -- we wanted Lkup c . Lkup ix but haskell complains
-  Tyof (f ': fam) (CofI Z      c) = Lkup c f
-  Tyof (f ': fam) (CofI (S ix) c) = Tyof fam (CofI ix c)
-  Tyof fam CofK        = '[]
+  Tyof (f ': codes) (CofI Z      c) = Lkup c f
+  Tyof (f ': codes) (CofI (S ix) c) = Tyof codes (CofI ix c)
+  Tyof codes CofK        = '[]
 
-data ES (ki :: kon -> *) (fam :: [[[Atom kon]]]) :: [Atom kon] -> [Atom kon] -> * where
-  Nil :: ES ki fam '[] '[]
-  Ins :: Cof ki fam a c 
-      -> ES ki fam i (Append (Tyof fam c) j)
-      -> ES ki fam i (a ': j)
-  Del :: Cof ki fam a c
-      -> ES ki fam (Append (Tyof fam c) i) j
-      -> ES ki fam (a ': i) j
-  Cpy :: Cof ki fam a c 
-      -> ES ki fam (Append (Tyof fam c) i) (Append (Tyof fam c) j)
-      -> ES ki fam (a ': i) (a ': j)
+data ES (ki :: kon -> *) (codes :: [[[Atom kon]]]) :: [Atom kon] -> [Atom kon] -> * where
+  Nil :: ES ki codes '[] '[]
+  Ins :: Cof ki codes a c 
+      -> ES ki codes i (Append (Tyof codes c) j)
+      -> ES ki codes i (a ': j)
+  Del :: Cof ki codes a c
+      -> ES ki codes (Append (Tyof codes c) i) j
+      -> ES ki codes (a ': i) j
+  Cpy :: Cof ki codes a c 
+      -> ES ki codes (Append (Tyof codes c) i) (Append (Tyof codes c) j)
+      -> ES ki codes (a ': i) (a ': j)
 
 
-data EST (ki :: kon -> *) (fam :: [[[Atom kon]]]) :: [Atom kon] -> [Atom kon] -> * where
-  NN :: ES  ki fam '[] '[] 
-     -> EST ki fam '[] '[]
-  NC :: Cof ki fam y c
-     -> ES  ki fam '[] (y ': ys)
-     -> EST ki fam '[] (Append (Tyof fam c) tys)
-     -> EST ki fam '[] (y ': tys)
-  CN :: Cof ki fam x c 
-     -> ES  ki fam (x ': xs) '[]
-     -> EST ki fam (Append (Tyof fam c) txs) '[]
-     -> EST ki fam (x ': txs) '[]
-  CC :: Cof ki fam x cx
-     -> Cof ki fam y cy
-     -> ES ki fam (x ': txs) (y ': tys)
-     -> EST ki fam (x ': txs) (Append (Tyof fam cy) tys)
-     -> EST ki fam (Append (Tyof fam cx) txs) (y ': tys)
-     -> EST ki fam (Append (Tyof fam cx) txs) (Append (Tyof fam cy) tys)
-     -> EST ki fam (x ': txs) (y ': tys)
+cost :: ES ki codes txs tys -> Int
+cost Nil = 0
+cost (Ins c es) = 1 + cost es
+cost (Del c es) = 1 + cost es
+cost (Cpy c es) = cost es
+
+meet :: ES ki codes txs tys -> ES ki codes txs tys -> ES ki codes txs tys
+meet d1 d2 = if cost d1 <= cost d2 then d1 else d2
+
+
+data EST (ki :: kon -> *) (codes :: [[[Atom kon]]]) :: [Atom kon] -> [Atom kon] -> * where
+  NN :: ES  ki codes '[] '[] 
+     -> EST ki codes '[] '[]
+  NC :: Cof ki codes y c
+     -> ES  ki codes '[] (y ': ys)
+     -> EST ki codes '[] (Append (Tyof codes c) tys)
+     -> EST ki codes '[] (y ': tys)
+  CN :: Cof ki codes x c 
+     -> ES  ki codes (x ': xs) '[]
+     -> EST ki codes (Append (Tyof codes c) txs) '[]
+     -> EST ki codes (x ': txs) '[]
+  CC :: Cof ki codes x cx
+     -> Cof ki codes y cy
+     -> ES ki codes (x ': txs) (y ': tys)
+     -> EST ki codes (x ': txs) (Append (Tyof codes cy) tys)
+     -> EST ki codes (Append (Tyof codes cx) txs) (y ': tys)
+     -> EST ki codes (Append (Tyof codes cx) txs) (Append (Tyof codes cy) tys)
+     -> EST ki codes (x ': txs) (y ': tys)
