6d45f244b7146e91928fb0d9ff5af8716f3f7483
Arian van Putten <aeroboy94@gmail.com>
2018-06-06 16:42:15 +0200

    Redo the Diff datatype. stuff was wrong

--- src/Generics/MRSOP/Diff.hs
+++ src/Generics/MRSOP/Diff.hs
@@ -8,21 +8,39 @@ import Generics.MRSOP.Base
 import Generics.MRSOP.Base (match)
 import Generics.MRSOP.Util
 
+-- Here,   Nat and Nat are indices that decide which SOP we're gonna use, within codes
+data Almu (ki :: kon -> *) (codes :: [[[Atom kon]]]) :: Nat -> Nat -> * where
+  Spn :: Spine (Lkup i codes)                                     -> Almu ki codes i i
+  Ins :: Constr (Lkup j codes) c -> Ctx ki codes j (Lkup c (Lkup i codes)) -> Almu ki codes i j
+  Del :: Constr (Lkup i codes) c -> Ctx ki codes i (Lkup c (Lkup j codes)) -> Almu ki codes i j
+
+ 
+data Ctx (ki :: kon -> *) (codes :: [[[Atom kon]]]) :: Nat -> [Atom kon] -> * where
+  CtxHere :: Almu ki codes i j -> PoA ki (Fix ki codes) prod -> Ctx ki codes i (I j ': prod)
+  
+data Spine (sum :: [[Atom kon]]) :: * where
+
+-- TODO we should mate Context a bit more normal formish, look at victor with it
 {- Once we fix this thing with generic zippers we can do this one instead
 data Almu :: Nat -> * where
   Peel :: {- TODO del and ins zippers  -> S (Lkup j fam) -} Almu i
 -}
 
 
+{-
+-- Where in the product do we insert a constructor
 data InsCtx :: Nat -> [Atom kon] -> * where
   InsHere :: Almu n2 n1 ->  InsCtx n2 (I n1 ': prod)
   InsThere :: NA (Fix ki codes) ki a -> InsCtx n prod -> InsCtx n (a ': prod)
 
+-- where in the product do we delete a constructor
 data DelCtx :: Nat -> [Atom kon] -> * where
   DelHere :: Almu n1 n2 ->  DelCtx n2 (I n1 ': prod)
   DelThere :: NA (Fix ki codes) ki a -> DelCtx n prod -> DelCtx n (a ': prod)
 
 
+data Almu :: 
+-- This Almu is wrong
 data Almu :: Nat -> Nat -> * where
   Spn :: S s -> Almu u u
   Ins :: Constr s v -> InsCtx u (Lkup v s) -> Almu u v
@@ -48,5 +66,6 @@ data S (sum :: [[Atom kon]]) :: * where
 
 
 sCns :: Constr sum n -> NP At (Lkup n sum) -> S sum
-sCns c x = SChg  c c (npToAl x)
+sCns c x = SChg c c (npToAl x)
 
+-}
