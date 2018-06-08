3a8687ba168a8b528884c0e5db283551218efc41
Arian van Putten <aeroboy94@gmail.com>
2018-06-08 12:52:34 +0200

    Hindent

--- src/Examples/TwoThreetree.hs
+++ src/Examples/TwoThreetree.hs
@@ -10,41 +10,82 @@
 {-# LANGUAGE TypeApplications #-}
 
 module Examples.TwoThreetree where
+
 import Data.Type.Equality
 
-import Generics.MRSOP.TH
-import Generics.MRSOP.Opaque
 import Generics.MRSOP.Base
-import Generics.MRSOP.Util
 import Generics.MRSOP.Diff
+import Generics.MRSOP.Opaque
+import Generics.MRSOP.TH
+import Generics.MRSOP.Util
+import qualified Generics.MRSOP.Zipper as Zipper
 
+data Tree a
+  = Leaf
+  | Two a
+        (Tree a)
+        (Tree a)
+  | Three a
+          (Tree a)
+          (Tree a)
+          (Tree a)
 
-data Tree a = Leaf | Two a (Tree a) (Tree a) | Three a (Tree a) (Tree a) (Tree a)
+data TreeKon =
+  TreeInt
 
-data TreeKon = TreeInt
 data TreeSingl (kon :: TreeKon) :: * where
   STreeInt :: Int -> TreeSingl TreeInt
 
 deriving instance Show (TreeSingl k)
+
 deriving instance Eq (TreeSingl k)
-instance Show1 TreeSingl where show1 = show
-instance Eq1 TreeSingl where eq1 = (==)
+
+instance Show1 TreeSingl where
+  show1 = show
+
+instance Eq1 TreeSingl where
+  eq1 = (==)
 
 instance TestEquality TreeSingl where
   testEquality (STreeInt _) (STreeInt _) = Just Refl
 
-deriveFamilyWith ''TreeSingl [t| Tree Int |]
+deriveFamilyWith ''TreeSingl [t|Tree Int|]
 
 t1, t2, t3, t4 :: Tree Int
 t1 = Three 1 Leaf (Two 2 Leaf Leaf) (Two 3 Leaf Leaf)
+
 t2 = Three 1 Leaf (Two 5 Leaf Leaf) (Two 3 Leaf Leaf)
+
 t3 = Three 1 Leaf (Two 2 Leaf Leaf) (Three 3 Leaf Leaf Leaf)
+
 t4 = Two 3 Leaf Leaf
 
 t1' = deep @FamTreeInt t1
+
 t2' = deep @FamTreeInt t2
+
 t3' = deep @FamTreeInt t3
+
 t4' = deep @FamTreeInt t4
 
-
-
+p12 :: Almu TreeSingl FamTreeInt CodesTreeInt Z
+p12 =
+  Peel
+    Zipper.Nil
+    Zipper.Nil
+    (sCns
+       (CS (CS CZ))
+       (AtSet (Trivial (STreeInt 1) (STreeInt 1)) :*
+        AtFix (Peel Zipper.Nil Zipper.Nil Scp) :*
+        AtFix
+          (Peel
+             Zipper.Nil
+             Zipper.Nil
+             (sCns
+                (CS CZ)
+                (AtSet (Trivial (STreeInt 2) (STreeInt 5)) :*
+                 AtFix (Peel Zipper.Nil Zipper.Nil Scp) :*
+                 AtFix (Peel Zipper.Nil Zipper.Nil Scp) :*
+                 NP0))) :*
+        AtFix (Peel Zipper.Nil Zipper.Nil Scp) :*
+        NP0))
