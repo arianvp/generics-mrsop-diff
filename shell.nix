86d14cdf3c03778b0bcb2689988a5f88c2ed9ea2
Arian van Putten <aeroboy94@gmail.com>
2018-08-05 15:22:55 +0200

    Nix shell. perhaps use add build tools in the future

---
+++ shell.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import <nixpkgs> {}}:
+let generics-mrsop-diff = import ./. {};
+in
+  generics-mrsop-diff.env.overrideAttrs (pkg : {
+    nativeBuildInputs = pkg.nativeBuildInputs ++ [
+      pkgs.haskellPackages.ghcid
+    ];
+  })
+  
