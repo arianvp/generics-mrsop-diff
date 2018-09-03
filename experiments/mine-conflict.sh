b7380c294da2e79438d2161b2bcc27bad32e11c8
Arian van Putten <a.vanputten@uu.nl>
2018-09-03 16:39:21 +0200

    Add better mine-conflict script that isnt dependend on currnet dir

---
+++ experiments/mine-conflict.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+GIT_WORK_TREE=""
+GIT_DIR=""
+
+repos="$1"
+target="$2"
+lang="$3"
+
+function earlyexit {
+  if ! [[ -z "$repo" ]]; then
+    git -C $repo merge --abort
+    git -C $repo reset -q --hard
+    git -C $repo clean -fdxq
+    git -C $repo checkout master
+    exit 1
+  fi
+}
+
+trap earlyexit SIGINT SIGTERM SIGQUIT
+
+for repo in $repos/*/; do
+  # make sure the git commits that follow are on the repo in question
+  # export GIT_WORK_TREE="$repo"
+  # export GIT_DIR="$repo/.git"
+
+  echo "Mining $repo"
+ 
+  old_branch=$(git -C $repo symbolic-ref --short HEAD)
+
+  for commit in $(git -C $repo rev-list --merges HEAD); do
+    parents=$(git -C $repo log -1 --format=%P $commit)
+    fst=${parents%% *}
+    rest=${parents#* }
+    git -C $repo checkout -q $fst
+    git -C $repo merge --no-commit $rest >/dev/null 2>&1
+    if git -C $repo ls-files --unmerged | grep -q '^'; then
+      echo "$found conflict in $repo - $commit"
+
+      unmerged=$(git -C $repo ls-files --unmerged | cut -d' ' -f2,3 | paste -d'\n' -s)
+      while read -r objID; do
+        obj=${objID:0:40}
+        role=${objID:41:1}
+        extension=${objID: -3}
+        if [ "$extension" == "$lang" ]; then
+          
+          if [ "$role" -eq "1" ]; then
+            fname="O.$lang"
+          elif [ "$role" -eq "2" ]; then
+            fname="A.$lang"
+          elif [ "$role" -eq "3" ]; then
+            fname="B.$lang"
+          fi
+          towrite="$target/$repo-$commit-$obj"
+          mkdir -p "$towrite"
+          git -C $repo cat-file -p $obj > "$towrite/$fname"
+        fi
+      done <<< $unmerged
+      git -C $repo merge --abort
+    fi
+    git -C $repo reset -q --hard
+    git -C $repo clean -fdxq
+  done
+  git -C $repo -q $old_branch
+done
+
