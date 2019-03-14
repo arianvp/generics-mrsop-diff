ee84a114beedb54410791512f5265cea1a393871
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2019-03-14 17:09:56 +0100

    The rest of the bugs

--- src/Generics/MRSOP/Diff3.hs
+++ src/Generics/MRSOP/Diff3.hs
@@ -137,71 +137,95 @@ data Spine (ki :: kon -> *) (codes :: [[[Atom kon]]]) :: [[Atom kon]] -> [[Atom 
     -> Al ki codes (Lkup c1 s1) (Lkup c2 s2)
     -> Spine ki codes s1 s2
 
+guard' :: String -> Bool -> Either String ()
+guard' s False = Left s
+guard' _ True  = Right ()
 
+instance Show1 SNat where
+  show1 = show . go
+    where
+     go :: SNat n -> Int
+     go SZ = 0
+     go (SS s) = 1 + go s
+
+instance Show1 (Constr r) where
+  show1 = showC
 
 applyAt
-  :: Eq1 ki
+  :: C ki
   => (At ki codes :*: NA ki (Fix ki codes)) a
-  -> Maybe (NA ki (Fix ki codes) a)
-applyAt (AtSet (Trivial a' b) :*: NA_K a) = 
-  guard (a `eq1` a') *> pure (NA_K b)
+  -> Either String (NA ki (Fix ki codes) a)
+applyAt (AtSet (Trivial a' b) :*: NA_K a)  
+  | eq1 a' b  = pure (NA_K a)
+  | eq1 a' a  = pure (NA_K b)
+  | otherwise = Left "atom"
 applyAt (AtFix x :*: NA_I x') = NA_I <$> applyAlmu x x'
 
 applyAl
-  :: Eq1 ki
+  :: C ki
   => Al ki codes xs ys
   -> PoA ki (Fix ki codes) xs
-  -> Maybe (PoA ki (Fix ki codes) ys)
-applyAl A0 NP0 = Just NP0
+  -> Either String (PoA ki (Fix ki codes) ys)
+applyAl A0 NP0 = return NP0
 applyAl (AX dx dxs) (x :* xs) =
   (:*) <$> applyAt (dx :*: x) <*> applyAl dxs xs
 applyAl (AIns x dxs) xs =
   (x :*) <$> applyAl dxs xs 
 applyAl (ADel x dxs) (x' :* xs) =
-  guard (eq1 x x') *> applyAl dxs xs
+  guard' "al del" (eq1 x x') *> applyAl dxs xs
+  -- applyAl dxs xs
 
+testEquality' :: (Show1 f , TestEquality f)
+              => f a -> f b -> Either String (a :~: b)
+testEquality' x y = case testEquality x y of
+  Just r -> Right r
+  Nothing -> Left ("te: " ++ show1 x ++ ", " ++ show1 y)
 
 applySpine 
-  :: Eq1 ki
+  :: C ki
   => SNat ix
   -> SNat iy
   -> Spine ki codes (Lkup ix codes) (Lkup iy codes)
   -> Rep ki (Fix ki codes) (Lkup ix codes)
-  -> Maybe (Rep ki (Fix ki codes) (Lkup iy codes))
-applySpine _ _ Scp x = Just x
+  -> Either String (Rep ki (Fix ki codes) (Lkup iy codes))
+applySpine _ _ Scp x = return x
 applySpine ix iy (SCns c1 dxs) (sop -> Tag c2 xs) =  do
-  Refl <- testEquality ix iy
-  Refl <- testEquality c1 c2
+  Refl <- testEquality' ix iy
+  Refl <- testEquality' c1 c2
   inj c2 <$> (mapNPM applyAt (zipNP dxs xs))
 applySpine _ _ (SChg c1 c2 al) (sop -> Tag c3 xs) = do
-  Refl <- testEquality c1 c3
+  Refl <- testEquality' c1 c3
   inj c2 <$> applyAl al xs
 
 insCtx
-  :: (IsNat ix, Eq1 ki)
+  :: (IsNat ix, C ki)
   => InsCtx ki codes ix xs
   -> Fix ki codes ix
-  -> Maybe (PoA ki (Fix ki codes) xs)
+  -> Either String (PoA ki (Fix ki codes) xs)
 insCtx (H x x2) x1 = (\x -> NA_I x :* x2) <$> applyAlmu x x1
 insCtx (T x x2) x1 = (x :*) <$> insCtx x2 x1
 
 delCtx
-  :: (Eq1 ki, IsNat ix)
+  :: (C ki, IsNat ix)
   => DelCtx ki codes ix xs
   -> PoA ki (Fix ki codes) xs
-  -> Maybe (Fix ki codes ix)
+  -> Either String (Fix ki codes ix)
 delCtx (H spu atmus) (NA_I x :* p) = applyAlmu (unAlmuMin spu) x
 delCtx (T atmu al) (at :* p) = delCtx al p
 
 applyAlmu 
-  :: forall ki codes ix iy. (IsNat ix, IsNat iy, Eq1 ki)
+  :: forall ki codes ix iy. (IsNat ix, IsNat iy, C ki)
   => Almu ki codes ix iy
   -> Fix ki codes ix
-  -> Maybe (Fix ki codes iy)
+  -> Either String (Fix ki codes iy)
 applyAlmu (Spn spine) (AnnFix _ rep) = Fix <$> applySpine (getSNat (Proxy @ix)) (getSNat (Proxy @iy)) spine rep
 applyAlmu (Ins c ctx) f@(AnnFix _ rep) = Fix . inj c <$> insCtx ctx f
-applyAlmu (Del c ctx) (AnnFix _ rep) = delCtx ctx <=< match c $ rep
+applyAlmu (Del c ctx) (AnnFix _ rep) = delCtx ctx <=< m2e . match c $ rep
+  where
+    m2e Nothing  = Left (show "del")
+    m2e (Just r) = Right r
 
+type C ki = (Eq1 ki , Show1 ki)
 
 -- it's only disjoint 
 
@@ -217,9 +241,18 @@ mergeAt (AtFix x) (AtFix y) = AtFix <$> mergeAlmu x y
 
 mergeAtAl :: Eq1 ki => NP (At ki codes) xs -> Al ki codes xs ys -> Maybe (Al ki codes xs ys)
 mergeAtAl NP0 A0 = pure A0
+-- potential: mergeAtAl [] (AIns at al) = AIns at A0
+--            mergeAtAl xs (AIns at al) = AIns at <$> mergeAtAl xs al
 mergeAtAl xs (AIns at al) = AIns at <$> mergeAtAl xs al
-mergeAtAl (x :* xs) (ADel at al) = ADel at <$> mergeAtAl xs al
+mergeAtAl (x :* xs) (ADel at al)
+  | identityAt x = ADel at <$> mergeAtAl xs al
+  | otherwise    = Nothing
 mergeAtAl (x :* xs) (AX at al) = AX <$> (mergeAt x at)  <*> mergeAtAl xs al
+
+identityAt :: (Eq1 ki) => At ki codes a -> Bool
+identityAt (AtFix (Spn Scp)) = True
+identityAt (AtSet (Trivial k1 k2)) = eq1 k1 k2
+identityAt _ = False
 
 
 makeIdAt :: NA ki (Fix ki codes) a -> At ki codes a
@@ -229,7 +262,9 @@ makeIdAt (NA_K k) = AtSet (Trivial k k)
 mergeAlAt :: Eq1 ki => Al ki codes xs ys -> NP (At ki codes) xs -> Maybe (NP (At ki codes) ys)
 mergeAlAt A0 NP0 = pure NP0
 mergeAlAt (AIns at al) xs = (makeIdAt at :*) <$> mergeAlAt al xs
-mergeAlAt (ADel at al) (x :* xs) = mergeAlAt al xs
+mergeAlAt (ADel at al) (x :* xs)
+  | identityAt x = mergeAlAt al xs
+  | otherwise    = Nothing
 mergeAlAt (AX a al)   (x :* xs) = (:*) <$> mergeAt a x <*> mergeAlAt al xs
 
 
@@ -278,6 +313,7 @@ mergeCtxAt (H (AlmuMin almu') rest) (AtFix almu :* xs) = do
   Refl <- testEquality (almuDest almu) (almuDest almu')
   x <- mergeAlmu almu' almu
   Refl <- testEquality (almuSrc x) (getSNat (Proxy @ix))
+  guard (and $ elimNP identityAt xs)
   pure x
 mergeCtxAt (T almu' ctx) (x :* xs) = mergeCtxAt ctx xs
 
@@ -285,6 +321,7 @@ mergeAtCtx :: (Eq1 ki, IsNat iy) => NP (At ki codes) xs -> DelCtx ki codes iy xs
 mergeAtCtx (AtFix almu :* xs) (H (AlmuMin almu') rest) = do
   Refl <- testEquality (almuDest almu) (almuDest almu')
   almu'' <- mergeAlmu almu almu'
+  guard (and $ elimNP identityAt xs)
   pure $ H  (AlmuMin almu'') rest
 mergeAtCtx (x :* xs) (T a  ctx) = do
    T a  <$> mergeAtCtx xs ctx
